ece1bb1788ac0fd02434f2e5e8fe50b9df0b912c
José Sabater Montes <jsm@iaa.es>
2019-02-14 13:56:35 +0000

    Correct units

--- repair_beam.py
+++ repair_beam.py
@@ -15,9 +15,9 @@ def repair_beam_info(fits_file, beam):
     """Add the beam information to the FITS file
     """
     with fits.open(fits_file, 'update') as f:
-        f[0].header['BPA'] = 0
-        f[0].header['BMAJ'] = beam
-        f[0].header['BMIN'] = beam
+        f[0].header['BPA'] = 0.
+        f[0].header['BMAJ'] = beam/3600.
+        f[0].header['BMIN'] = beam/3600.
 
 
 if __name__ == "__main__":
